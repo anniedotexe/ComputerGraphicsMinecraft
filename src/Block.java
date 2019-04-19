410811179cc84528fad8a29d10afc5a860f5b71d
Annie <anniewu2303@gmail.com>
2019-04-18 22:22:22 -0700

    Update Comment Blocks for Methods

--- src/Block.java
+++ src/Block.java
@@ -31,39 +31,71 @@ public class Block {
         BlockType(int i) {
             BlockID = i;
         }
-        
+
         public int getID() {
             return BlockID;
         }
-        
+
         public void setID(int i) {
             BlockID = i;
         }
     }
     
+    /**
+     * Constructor: Block 
+     * Purpose: Create a block of this type 
+     * @param type 
+     */
     public Block(BlockType type) {
         Type = type;
         isActive = false;
     }
 
+    /**
+     * Method: setCoords
+     * Purpose: Set coordinates of the block
+     * @param x
+     * @param y
+     * @param z 
+     */
     public void setCoords(float x, float y, float z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    public boolean isActive(){
+    /**
+     * Method: isActive
+     * Purpose: Return true if the block is active 
+     * @return 
+     */
+    public boolean isActive() {
         return isActive;
     }
 
+    /**
+     * Method: setActive
+     * Purpose: Set a block to be active or not active
+     * @param active 
+     */
     public void setActive(boolean active) {
         isActive = active;
     }
 
+    /**
+     * Method: getID
+     * Purpose: Get the ID of a block
+     * @return 
+     */
     public int getID() {
         return Type.getID();
     }
 
+    /**
+     * Method: setID
+     * Purpose: Set the ID of a block
+     * @param type 
+     */
     public void setID(BlockType type) {
         Type = type;
     }
